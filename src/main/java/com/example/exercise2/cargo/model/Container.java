30eadb395351a538e1ba4994f0812512f195b415
AQUINHI <hikarutaquino@gmail.com>
2025-08-19 14:32:40 +0800

    Added REST API endpoints, controllers, service, repository and model

---
+++ src/main/java/com/example/exercise2/cargo/model/Container.java
@@ -0,0 +1,26 @@
+package com.example.exercise2.cargo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Container {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private double weight; // in kg
+    private String origin;
+    private String destination;
+}
+
+
+
